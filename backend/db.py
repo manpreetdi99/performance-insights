c35634523830c971ceaff458f0705a21c25a8990
drivetestteams <61454143+manpreetdi99@users.noreply.github.com>
2026-03-27 13:16:31 +0200

    fix database

--- backend/db.py
+++ backend/db.py
@@ -4,21 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SERVER = os.getenv("DB_SERVER")
-USER = os.getenv("DB_USER")
-PASSWORD = os.getenv("DB_PASS")
+SERVER = os.getenv("DB_HOST", "swissqual-srvsa")
+USER = os.getenv("DB_USER", "sa")
+PASSWORD = os.getenv("DB_PASS", "test123@")
 DRIVER = os.getenv("DB_DRIVER", "ODBC Driver 17 for SQL Server")
-DB_PREFIX = os.getenv("DB_PREFIX", "NMS_")
+DB_PORT = os.getenv("DB_PORT", "1433")
+
 
 def get_connection(database_name: str):
     return pyodbc.connect(
         f"DRIVER={{{DRIVER}}};"
-        f"SERVER={SERVER};"
+        f"SERVER={SERVER},{DB_PORT};"
         f"DATABASE={database_name};"
         f"UID={USER};"
         f"PWD={PASSWORD};"
-        "TrustServerCertificate=yes;"
+        "TrustServerCertificate=yes;",
+        timeout=5
     )
+
 
 def get_available_databases():
     conn = get_connection("master")
@@ -27,9 +30,12 @@ def get_available_databases():
         SELECT name
         FROM sys.databases
         WHERE state_desc = 'ONLINE'
-          AND name LIKE ?
         ORDER BY name
-    """, f"{DB_PREFIX}%")
+    """)
     rows = cursor.fetchall()
     conn.close()
     return [row[0] for row in rows]
+
+
+if __name__ == "__main__":
+    print(get_available_databases())
